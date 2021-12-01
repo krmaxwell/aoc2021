8e97b6eb315d290c138c95e40a68cf77708e8661
Kyle Maxwell <kylemxl@icloud.com>
2021-12-01 11:16:24 -0600

    Full day 1 completion

--- day1/day1.go
+++ day1/day1.go
@@ -1,6 +1,6 @@
 package day1
 
-func SonarSweep(data []int) int {
+func SonarSweepNaive(data []int) int {
 	decreaseCount := 0
 	for i := 1; i < len(data); i++ {
 		if data[i] > data[i-1] {
@@ -8,4 +8,14 @@ func SonarSweep(data []int) int {
 		}
 	}
 	return decreaseCount
+}
+
+func SonarSweepSlidingSum(data []int) int {
+	var sums []int
+	var newSum int
+	for i := 0; i < len(data)-2; i++ {
+		newSum = data[i] + data[i+1] + data[i+2]
+		sums = append(sums, newSum)
+	}
+	return SonarSweepNaive(sums)
 }
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestSonarSweep(t *testing.T) {
-	t.Run("initial test data", func(t *testing.T) {
+	t.Run("naive sweep with test data", func(t *testing.T) {
 		reportData := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
-		got := SonarSweep(reportData)
+		got := SonarSweepNaive(reportData)
 		want := 7
 
 		if got != want {
@@ -19,29 +19,51 @@ func TestSonarSweep(t *testing.T) {
 		}
 	})
 
-	t.Run("full data", func(t *testing.T) {
+	t.Run("naive sweep with full data", func(t *testing.T) {
 		// this isn't really good TDD practice
 		// but that's not the point of this exercise anyway
-		f, err := os.Open("day1.dat")
-		if err != nil {
-			t.Fatalf("Could not open day1.dat: %v", err)
-		}
-
-		defer f.Close()
-
-		scanner := bufio.NewScanner(f)
-		reportData := []int{}
-		var datum int
-
-		for scanner.Scan() {
-			datum, err = strconv.Atoi(scanner.Text())
-			if err != nil {
-				t.Fatalf("Could not convert datum: %v", err)
-			}
-			reportData = append(reportData, datum)
-		}
-
-		got := SonarSweep(reportData)
+		reportData := readFullData(t, "day1.dat")
+		got := SonarSweepNaive(reportData)
 		fmt.Printf("Counted %d decreases\n", got)
 	})
+
+	t.Run("sliding sweep with test data", func(t *testing.T) {
+		reportData := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+		got := SonarSweepSlidingSum(reportData)
+		want := 5
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("sliding sweep with full data", func(t *testing.T) {
+		reportData := readFullData(t, "day1.dat")
+		got := SonarSweepSlidingSum(reportData)
+		fmt.Printf("Counted %d decreases\n", got)
+	})
+}
+
+func readFullData(t *testing.T, fname string) []int {
+	t.Helper()
+	f, err := os.Open(fname)
+	if err != nil {
+		t.Fatalf("Could not open %q: %q", fname, err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	reportData := []int{}
+	var datum int
+
+	for scanner.Scan() {
+		datum, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			t.Fatalf("Could not convert datum: %v", err)
+		}
+		reportData = append(reportData, datum)
+	}
+
+	return reportData
 }
