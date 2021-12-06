74b3d1058c76fa89b6ccdd517e4f9ecbedf32028
Kyle Maxwell <kylemxl@icloud.com>
2021-12-06 08:25:50 -0600

    Refactor for efficiency and part 2

--- day6/day6.go
+++ day6/day6.go
@@ -1,17 +1,24 @@
 package day6
 
-func simulateDays(fish []int, days int) int {
-	for day := 0; day < days; day++ {
-		//fmt.Println(fish)
-		fishLen := len(fish)
-		for f := 0; f < fishLen; f++ {
-			if fish[f] > 0 {
-				fish[f]--
-			} else if fish[f] == 0 {
-				fish = append(fish, 8)
-				fish[f] = 6
-			}
-		}
+func simulateDays(fish []int, days int) int64 {
+	todaysFish := [9]int64{}
+
+	// count existing fish by days remaining to reproduce
+	for f := 0; f < len(fish); f++ {
+		todaysFish[fish[f]]++
 	}
-	return len(fish)
+
+	for day := 0; day < days; day++ {
+		reproduction := todaysFish[0]
+		for i := 0; i < 8; i++ {
+			todaysFish[i] = todaysFish[i+1]
+		}
+		todaysFish[6] += reproduction
+		todaysFish[8] = reproduction
+	}
+	var sum int64
+	for i := 0; i < 9; i++ {
+		sum += todaysFish[i]
+	}
+	return sum
 }
