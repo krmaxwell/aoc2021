a0e0bc1c9c55eed69d5b20fa55482f46d49faec8
Kyle Maxwell <kylemxl@icloud.com>
2021-12-07 09:46:59 -0600

    Part 2 complete

--- day7/day7.go
+++ day7/day7.go
@@ -23,6 +23,25 @@ func MinimumFuel(positions []int) int {
 	return minfuel
 }
 
+func AccurateMinimumFuel(positions []int) int {
+	minfuel := math.MaxInt
+	maxPosition := maxInt(positions)
+	fuel := make([]int, maxPosition+1)
+	for pos := 0; pos <= maxPosition; pos++ {
+		for _, crab := range positions {
+			if crab > pos {
+				fuel[pos] += triangleNumber(crab - pos)
+			} else {
+				fuel[pos] += triangleNumber(pos - crab)
+			}
+		}
+		if fuel[pos] < minfuel {
+			minfuel = fuel[pos]
+		}
+	}
+	return minfuel
+}
+
 func maxInt(data []int) int {
 	max := 0
 	for i := range data {
@@ -31,4 +50,8 @@ func maxInt(data []int) int {
 		}
 	}
 	return max
+}
+
+func triangleNumber(n int) int {
+	return n * (n + 1) / 2
 }
